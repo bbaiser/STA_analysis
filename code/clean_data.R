72bff1ebc03048557f38c66ad5bbdf385b124eac
bbaiser <benj.baiser@gmail.com>
2021-03-16 14:53:56 -0400

    filter data down to 4 stas for a common time period (5/2006 to 4/2020

---
+++ code/clean_data.R
@@ -0,0 +1,39 @@
+#Packages
+
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(ggplot2)
+
+d<-c(1,1,1,1,2,2,2,3,3,3)
+e<-c(1,2,3,4,1,2,3,1,2,3)
+f<-cbind(d,e)
+
+
+#complete data frame compiled by Jing HU on 3/2/2021
+master_dat<-read.csv("data/R_sta_month.csv")
+
+colnames(master_dat)
+unique(master_dat$sta)
+
+#make a list of STAs that we will be analyzing
+sta_red<-c("sta_1E", "sta_1W", "sta_2",  "sta_34")
+
+sta_4 <- master_dat %>%
+  filter(sta %in% sta_red) %>%
+  filter(year > 2006)  #take years 2006 forward
+  
+sta_5 <- master_dat %>%
+  filter(sta %in% sta_red) %>%
+  filter(year==2006 & month >4)%>%
+  bind_rows(sta_4)%>%
+  arrange(.,X)
+
+
+13*12*4
+624+16
+ str(sta_4) 
+
+ sta_4 <- master_dat %>%
+   filter(sta %in% sta_red) %>%
+   which(year > 2005 & month >4) %>%
