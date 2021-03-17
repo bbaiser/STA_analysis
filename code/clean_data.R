fa1e3583cd20293b5ba06c8b64ed629d03ee0eda
bbaiser <benj.baiser@gmail.com>
2021-03-17 08:51:20 -0400

    save out the file ready to use for analysis

--- code/clean_data.R
+++ code/clean_data.R
@@ -5,29 +5,31 @@ library(tidyr)
 library(plyr)
 library(ggplot2)
 
-d<-c(1,1,1,1,2,2,2,3,3,3)
-e<-c(1,2,3,4,1,2,3,1,2,3)
-f<-cbind(d,e)
+#import data and clean data####
+
+master_dat<-read.csv("data/R_sta_month.csv") #complete data frame compiled by Jing HU on 3/2/2021
+
+head(master_dat)
 
 
-#complete data frame compiled by Jing HU on 3/2/2021
-master_dat<-read.csv("data/R_sta_month.csv")
-
-colnames(master_dat)
-unique(master_dat$sta)
-
-#make a list of STAs that we will be analyzing
+#make a list of STAs that we will be analyzing 
 sta_red<-c("sta_1E", "sta_1W", "sta_2",  "sta_34")
 
+#remove stas and years we will not analyze
 sta_4 <- master_dat %>%
-  filter(sta %in% sta_red) %>%
+  filter(sta %in% sta_red) %>%   # remove sta 5,6 and 5/6
   filter(year > 2006)  #take years 2006 forward
-  
-sta_5 <- master_dat %>%
-  filter(sta %in% sta_red) %>%
-  filter(year==2006 & month >4)%>%
-  bind_rows(sta_4)%>%
-  arrange(.,X)
 
+#remove the first 4 months of 2006 for three of the four sta's to standardize period of record  
+sta_5 <- master_dat %>%
+  filter(sta %in% sta_red) %>% # remove sta 5,6 and 5/6
+  filter(year==2006 & month >4)%>% #now only take month 5-12 for 2006 from the remaining stas
+  bind_rows(sta_4)%>% #combine the 2006 data with the 2007-2020 data (sta4) from above
+  arrange(.,X) #reorder
+
+head(sta_5)
+
+#save reduced data frame out to data folder
+write.csv(sta_5, "data/4_sta.csv")
 
 
