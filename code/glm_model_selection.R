761110c64caecafcc378ad0efc1f4b7cddc53ef4
bbaiser <benj.baiser@gmail.com>
2021-03-17 09:56:53 -0400

    script to select best model for each sem "piece"

---
+++ code/glm_model_selection.R
@@ -0,0 +1,42 @@
+####Model selection for 4 STA analysis (2006-2020)
+
+#Packages
+
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(ggplot2)
+library(nlme)
+library(lme4)
+library(lmtest)
+library(MuMIn) 
+library(lme4)
+
+
+#import data####
+
+full_dat<-read.csv("data/4_sta.csv") #complete data frame compiled by Jing HU on 3/2/2021
+
+head(full_dat)
+
+#build model "piece" predicting total p outfloc conc. (out_tp_c )
+
+# out_tp_c =total p outfloc conc.
+# tp_rr = total p retention rate
+# in_tp_c =total p inflow conc.
+
+#lme model
+tp_out <-lme(out_tp_c ~ tp_rr+ in_tp_c, random = ~ 1 | sta, data = full_dat, na.action = na.exclude)
+
+summary(tp_out)
+r.squaredGLMM(tp_out)
+
+tp_out <-lme(out_tp_c ~ in_tp_c, random = ~ 1 | sta, data = full_dat, na.action = na.exclude)
+
+summary(tp_out)
+r.squaredGLMM(tp_out)
+
+??r2_nakagawa
+E1<-resid(prey, type="pearson")
+plot(x=prey,y=E1)
+
