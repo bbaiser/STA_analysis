fa66a8ca62f24a4c899a73345c5a377bbe47ad00
bbaiser <benj.baiser@gmail.com>
2021-03-23 07:11:18 -0400

    remove nas at the beggining and add single time series plots

--- code/glm_model_selection.R
+++ code/glm_model_selection.R
@@ -15,10 +15,13 @@ library(lme4)
 
 #import data####
 
-full_dat<-read.csv("data/4_sta.csv", row=1) #complete data frame compiled by Jing HU on 3/2/2021
+NA_dat<-read.csv("data/4_sta.csv", row=1) #complete data frame compiled by Jing HU on 3/2/2021
 
-head(full_dat)
+head(NA_dat)
 
+full_dat<-NA_dat %>%
+  select(out_tp_c,in_tp_c,tp_rr, sta,por2)%>%
+  filter(complete.cases(.))
 
 #???????????????????remove na before everything?
 
@@ -50,6 +53,40 @@ sep_sta<-ggplot(p_out_dat, aes(x=por2, y=out_tp_c, group=sta)) +
 
 sep_sta
 
+#1E
+sep_sta_1E<-ggplot(subset(p_out_dat,sta %in% c("sta_1E")), aes(x=por2, y=out_tp_c)) +
+  geom_line(aes(color=sta))
+
+sep_sta_1E
+
+#1W
+sep_sta_1W<-ggplot(subset(p_out_dat,sta %in% c("sta_1W")), aes(x=por2, y=out_tp_c)) +
+  geom_line(aes(color=sta))
+
+sep_sta_1W
+
+#1W
+sep_sta_1W<-ggplot(subset(p_out_dat,sta %in% c("sta_1W")), aes(x=por2, y=out_tp_c)) +
+  geom_line(aes(color=sta))
+
+sep_sta_1W
+
+
+#2
+sep_sta_2<-ggplot(subset(p_out_dat,sta %in% c("sta_2")), aes(x=por2, y=out_tp_c)) +
+  geom_line(aes(color=sta))
+
+
+sep_sta_2
+
+#3/4
+
+sep_sta_3_4<-ggplot(subset(p_out_dat,sta %in% c("sta_34")), aes(x=por2, y=out_tp_c)) +
+  geom_line(aes(color=sta))
+
+
+sep_sta_3_4
+
 # all data combined time series
 tog_sta<-ggplot(p_out_dat, aes(x=por2, y=out_tp_c, group=1)) +
   geom_line()
@@ -58,7 +95,7 @@ tog_sta
 
 
 #lme model with sta as a random effect - need to log transform to even remotely meet assumptions
-tp_out <-lme(log(out_tp_c) ~ tp_rr+ in_tp_c, random = ~ 1 | sta, data = p_out_dat, na.action = na.exclude)
+tp_out <-lme(log(out_tp_c) ~ tp_rr+ in_tp_c, random = ~ 1 | sta, data = p_out_dat)
 
 summary(tp_out)
 r.squaredGLMM(tp_out) 
@@ -77,17 +114,43 @@ acf(E1,na.action = na.exclude)
 pacf(E1,na.action = na.exclude)
 
 
+
+
+
+#test p, q for corARMA ####
+
+cor.results <- NULL
+for(i in 0:2) {
+  for(j in 0:2) {
+    if(i>0 | j>0) {
+      tp_out_ARMA <-lme(log(out_tp_c) ~ 
+                          tp_rr + 
+                          in_tp_c, 
+                        random = ~ 1 | sta,
+                        correlation = corARMA(form = ~ 1 | sta, p = i, q = j),
+                        data = p_out_dat, 
+                        na.action = na.exclude)
+      cor.results<-as.data.frame(rbind(cor.results,c(i, j, AIC(tp_out_ARMA))))
+    }
+  }
+}
+
+colnames(cor.results) <- c('i', 'j', 'AIC')
+cor.results %>% arrange(AIC)
+
+
 #lme model with ARMA
 #lme model with sta as a random effect - need to log transform to even remotely meet assumptions
-tp_out_ARMA <-lme(log(out_tp_c) ~ 
+tp_out_ARMA <-lme(out_tp_c ~ 
                     tp_rr + 
                     in_tp_c, 
                   random = ~ 1 | sta,
-                  correlation = corARMA(form = ~ 1 | sta, p = 2, q=2 ),
-                  data = p_out_dat, na.action = na.exclude)
+                  correlation = corARMA(form = ~ 1 | sta, p = 1, q=2 ),
+                  data = p_out_dat)
 
-
-
+zz<-diff(p_out_dat$out_tp_c,lag=4)
+Box.test(zz, lag=20, type="Ljung-Box")#test for autocorr
+acf(p_out_dat$out_tp_c)
 
 summary(tp_out_ARMA)
 r.squaredGLMM(tp_out_ARMA) 
