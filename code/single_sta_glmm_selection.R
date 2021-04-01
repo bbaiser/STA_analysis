91b8a770e8b3dba1847f6fc711cbb6b513444d5f
bbaiser <benj.baiser@gmail.com>
2021-04-01 09:59:15 -0400

    added ggplot for univraite regressions

--- code/single_sta_glmm_selection.R
+++ code/single_sta_glmm_selection.R
@@ -48,7 +48,8 @@ p_out_dat<- full_dat %>%
 
 #look time series for STA2
 sep_sta_2<-ggplot(subset(p_out_dat,sta %in% c("sta_2")), aes(x=por2, y=out_tp_c)) +
-  geom_line(aes(color=sta))
+  geom_line(aes(color=sta))+
+  xlab("Period of Record (Month)") + ylab("Total P output conc.")
 
 
 sep_sta_2
@@ -69,6 +70,12 @@ tp_out <-lm(log(out_tp_c) ~  in_tp_c+tp_rr, data = sta2)
 tp_out <-lm(log(out_tp_c) ~  tp_rr, data = sta2)
 tp_out <-lm(log(out_tp_c) ~  in_tp_c, data = sta2)
 
+
+#plot
+ggplot(sta2,aes(tp_rr, log(out_tp_c))) +
+  stat_summary(fun.data=mean_cl_normal) + 
+  geom_smooth(method='lm', formula= y~x)
+
 #check multicollinearity
 car::vif(lm(log(out_tp_c) ~  in_tp_c+tp_rr, data = sta2))
 
@@ -87,3 +94,53 @@ E1<-resid(tp_out, type="pearson")
 Box.test(resid(tp_out), lag=20, type="Ljung-Box")#test for autocorr
 acf(E1)
 pacf(E1)
+
+#######lme model WITH Correlation P = 1
+tp_out_ARMA <-gls(log(out_tp_c) ~  in_tp_c, data = sta2,correlation = corARMA(p = 0, q = 1,form =~ 1))
+#simple regression
+tp_out <-lm(log(out_tp_c) ~  tp_rr, data = sta2)
+tp_out <-lm(log(out_tp_c) ~  in_tp_c, data = sta2)
+
+#check multicollinearity
+car::vif(lm(log(out_tp_c) ~  in_tp_c+tp_rr, data = sta2))
+
+#check summary 
+summary(tp_out_ARMA)
+r.squaredLR(tp_out_ARMA) 
+
+#check residuals
+plot(tp_out_ARMA)
+
+# extract model residuals
+E1<-resid(tp_out_ARMA, type="pearson")
+
+
+#explore temporal autocorrelation
+Box.test(resid(tp_out_ARMA), lag=20, type="Ljung-Box")#test for autocorr
+acf(E1)
+pacf(E1)
+
+
+cor.results <- NULL
+for(i in 0:2) {
+  for(j in 0:2) {
+    if(i>0 | j>0) {
+      tp_out_ARMA <-gls(log(out_tp_c) ~  
+                         in_tp_c+
+                         tp_rr, 
+                        correlation = corARMA(p = i, q = j),
+                        data = p_out_dat, 
+                        na.action = na.exclude)
+      cor.results<-as.data.frame(rbind(cor.results,c(i, j, AIC(tp_out_ARMA))))
+    }
+  }
+}
+require(forecast)    
+
+x <- as.matrix(sta2[,c("tp_rr","in_tp_c")])
+fit3 <- auto.arima(log(sta2$out_tp_c), xreg=x)
+auto.arima(sta2$out_tp_c)
+summary(fit3)
+
+colnames(cor.results) <- c('i', 'j', 'AIC')
+cor.results %>% arrange(AIC)
