836c82559a12a141353be896ab5139e6aca097c1
bbaiser <benj.baiser@gmail.com>
2021-04-01 15:55:53 -0400

    added interpolation and decomposition of time series

--- code/detrend_decycle.R
+++ code/detrend_decycle.R
@@ -11,7 +11,7 @@ library(lme4)
 library(lmtest)
 library(MuMIn) 
 library(lme4)
-
+library(zoo)
 
 #import data####
 
@@ -20,8 +20,8 @@ NA_dat<-read.csv("data/4_sta.csv", row=1) #complete data frame compiled by Jing 
 head(NA_dat)
 
 full_dat<-NA_dat %>%
-  select(out_tp_c,in_tp_c,tp_rr, sta,por2)%>%
-  filter(complete.cases(.))
+  select(out_tp_c,in_tp_c,tp_rr, sta,por2) 
+#%>%filter(complete.cases(.))
 
 #???????????????????remove na before everything?
 
@@ -47,27 +47,45 @@ p_out_dat<- full_dat %>%
 
 
 #look time series for STA2
-sep_sta_2<-ggplot(subset(p_out_dat,sta %in% c("sta_2")), aes(x=por2, y=out_tp_c)) +
-  geom_line(aes(color=sta))
+#subset for sta2
+sta2<-subset(full_dat,sta %in% c("sta_2"))%>%
+mutate(out_tp_c_int=na.approx(sta2$out_tp_c,sta2$por2))#interpolate over 6 nas
 
+count(is.na(subset(full_dat,sta %in% c("sta_2"))$out_tp_c))#of nas in out_tp_c
+
+#plot time series
+sep_sta_2<-ggplot(sta2, aes(x=por2, y=out_tp_c_int)) +
+  geom_line(aes(color=sta))+xlab("Period of Record (Month)") + 
+  ylab("Total [P] output")
 
 sep_sta_2
 
+
+
+
+
+
 #test for autocorrelation
-acf(subset(p_out_dat,sta %in% c("sta_2"))$out_tp_c)
-pacf(subset(p_out_dat,sta %in% c("sta_2"))$out_tp_c)
-Box.test(subset(p_out_dat,sta %in% c("sta_2"))$out_tp_c, lag=20, type="Ljung-Box")
-
-#suset sta2 for the model
-sta2<-p_out_dat%>%
-  filter(sta %in% c("sta_2")) 
+acf(sta2$out_tp_c_int)
+pacf(sta2$out_tp_c_int)
+Box.test(sta2$out_tp_c_int, lag=20, type="Ljung-Box")
 
 
-ts(sta2$out_tp_c)
+#make actual timeseries object
 
-p<-ts(sta2$out_tp_c, frequency = 12)
-plot.ts(p)
-dp<-decompose(p)
+sta2_ts<-ts(sta2$out_tp_c_int, frequency = 12)#note that May is the first month
+plot.ts(sta2_ts)
+sta_comps<-decompose(sta2_ts)#decompose into trend, season and noise
 
-plot(dp)
+plot(sta_comps)#plot decomposition components
 
+
+
+sta2_seas_adj <- sta2_ts-sta_comps$seasonal
+
+plot(sta2_seas_adj)
+acf(sta2_seas_adj)
+diff<-diff(sta2_seas_adj,difference=1)
+plot.ts(diff)
+pacf(sta2_seas_adj,lag.max=20)
+acf(sta2_seas_adj,lag.max=20)
