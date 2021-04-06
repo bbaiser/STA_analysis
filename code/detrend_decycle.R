c48bbfbf6c6236bc9b84c088f5d7060851cfec12
bbaiser <benj.baiser@gmail.com>
2021-04-06 09:57:55 -0400

    intepolating all thre varibles and trying to use arima multiple regression

--- code/detrend_decycle.R
+++ code/detrend_decycle.R
@@ -40,23 +40,47 @@ boxplot(full_dat$tp_rr)
 
 
 #remove the outlier/error from the tp_out (point 225)
-p_out_dat<- full_dat %>%
-  filter(out_tp_c < 0.2)  
+#p_out_dat<- full_dat %>%
+#filter(out_tp_c < 0.2)  
 
 
 
 
 #look time series for STA2
 #subset for sta2
-sta2<-subset(full_dat,sta %in% c("sta_2"))%>%
-mutate(out_tp_c_int=na.approx(sta2$out_tp_c,sta2$por2))#interpolate over 6 nas
+sta2<-subset(full_dat,sta %in% c("sta_2"))
 
-count(is.na(subset(full_dat,sta %in% c("sta_2"))$out_tp_c))#of nas in out_tp_c
+
+#interpolate using splines for each varaible
+sta2_int<-sta2%>%
+      mutate(out_tp_c_int=na.spline(sta2$out_tp_c,sta2$por2))%>%#interpolate over 6 nas
+      mutate(in_tp_c_int=na.spline(sta2$in_tp_c,sta2$por2))%>%#interpolate over 1 na
+      mutate(tp_rr_int=na.spline(sta2$tp_rr,sta2$por2))#no na
+
+#of nas in out_tp_c
+count(is.na(subset(full_dat,sta %in% c("sta_2"))$tp_rr))#of nas in out_tp_c
 
 #plot time series
-sep_sta_2<-ggplot(sta2, aes(x=por2, y=out_tp_c_int)) +
+
+#tp_out
+sep_sta_2<-ggplot(sta2_int, aes(x=por2, y=out_tp_c_int)) +
   geom_line(aes(color=sta))+xlab("Period of Record (Month)") + 
   ylab("Total [P] output")
+
+sep_sta_2
+
+#tp_in
+sep_sta_2<-ggplot(sta2_int, aes(x=por2, y=in_tp_c_int)) +
+  geom_line(aes(color=sta))+xlab("Period of Record (Month)") + 
+  ylab("Total [P] input")
+
+sep_sta_2
+
+
+#tp_rr
+sep_sta_2<-ggplot(sta2_int, aes(x=por2, y=tp_rr_int)) +
+  geom_line(aes(color=sta))+xlab("Period of Record (Month)") + 
+  ylab("Total P retention rate")
 
 sep_sta_2
 
@@ -66,20 +90,31 @@ sep_sta_2
 
 
 #test for autocorrelation
-acf(sta2$out_tp_c_int)
-pacf(sta2$out_tp_c_int)
-Box.test(sta2$out_tp_c_int, lag=20, type="Ljung-Box")
+acf(sta2_int$out_tp_c_int)
+pacf(sta2_int$out_tp_c_int)
+Box.test(sta2_int$out_tp_c_int, lag=20, type="Ljung-Box")
 
-(fit <- Arima(sta2$out_tp_c_int, order=c(0,0,1)))
+
+
+
+#auto arima
+auto.arima(sta2_int$out_tp_c_int, trace=T)
+
+fit <- Arima(sta2_int$out_tp_c_int, order=c(0,0,1)))
+pacf(residuals(fit))
 acf(residuals(fit))
 
+plot(residuals(fit))
 
-auto.arima(sta2$out_tp_c_int)
 
+tp_out <-lm(log(out_tp_c_int) ~  in_tp_c, data = sta2_int)
+     
+     
+sta2_int$in_tp_c   
+     
+#make actual timeseries object####
 
-#make actual timeseries object
-
-sta2_ts<-ts(sta2$out_tp_c_int, frequency = 12)#note that May is the first month
+sta2_ts<-ts(residuals(fit), frequency = 12)#note that May is the first month
 plot.ts(sta2_ts)
 sta_comps<-decompose(sta2_ts)#decompose into trend, season and noise
 
