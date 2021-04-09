366690843f4dddb238a4851e73e8deeea6cf4f94
bbaiser <benj.baiser@gmail.com>
2021-04-09 15:02:03 -0400

    interpolate all varibles at once, maybe integrate into the other data clean script when done

---
+++ code/clean_data_pt2.R
@@ -0,0 +1,8 @@
+#interpolate all varaibles for sta2 with spline
+
+NA_dat<-read.csv("data/4_sta.csv", row=1) #complete data frame compiled by Jing HU on 3/2/2021
+
+head(NA_dat)
+
+mod_vars<-NA_dat %>%
+  select(out_tp_c,in_tp_c,tp_rr,in_tn_c,tn_rr,in_ca_c,ca_rr,in_water_l,temp_mean, rainfall_mean,sta,por2, month, year) 
