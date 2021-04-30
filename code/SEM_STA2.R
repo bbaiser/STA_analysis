09595f5b6208cab9b0a4506792140ecc60f595d0
bbaiser <benj.baiser@gmail.com>
2021-04-30 08:44:13 -0400

    Updated with model from the

--- code/SEM_STA2.R
+++ code/SEM_STA2.R
@@ -24,7 +24,7 @@ sta2<-subset(NA_dat,sta %in% c("sta_2"))
 
 #select variables used in model (missing VEG as of 4/26/21; check for others)
 mod_vars<-sta2 %>%
-  select(out_tp_c,in_tp_c,tp_rr,in_tn_c,tn_rr,in_ca_c,ca_rr,in_water_l,temp_mean, rainfall_mean,sta,por2, month, year, HRT, HRT_365)
+  select(out_tp_c,in_tp_c,tp_rr,in_tn_c,tn_rr,in_ca_c,ca_rr,in_water_l,temp_mean, rainfall_mean,sta,por2, month, year, HRT, wd_mean, out_water_l, wd_0_mean)
 
 
 #interpolate using splines for each variable. some don;t have nas and dont need it
@@ -40,14 +40,15 @@ sta2_int<-mod_vars%>%
   mutate(temp_mean_int=na.spline(sta2$temp_mean,sta2$por2))%>% #no na
   mutate(rainfall_mean_int=na.spline(sta2$rainfall_mean,sta2$por2))%>%#no na
   mutate(HRT_int=na.spline(sta2$HRT, sta2$por2))%>%#6 NA
-  mutate(HRT365_int=na.spline(sta2$HRT_365, sta2$por2))#6NA
+  mutate(wd_mean_int=na.spline(sta2$wd_mean, sta2$por2))%>%#6NA
+  mutate(wd_0_mean_int=na.spline(sta2$wd_0_mean, sta2$por2))%>%#no na
+  mutate(out_water_int=na.spline(sta2$out_water_l, sta2$por2))#no na
 
 sta2_int$HRT_int[sta2_int$HRT_int<0] <- 0#replace ridiculous neg interpolated value with 0
 
 
 
-
-#models: selected using aic for p and q fro script STA_2_models.R####
+#models: selected using aic for p and q fro script STA_2_models.R with HRT####
 
 TP_outflow_mod <-gls(log(out_tp_c_int)  ~ 
                  in_tp_c_int+
@@ -101,8 +102,66 @@ TCA_RR_mod <-gls(ca_rr_int~
 
 
 
-####Path for MPD as response#### 
-model1<-psem(TN_RR_mod,TCA_RR_mod,HRT_mod,TP_RR_mod,TP_outflow_mod)
+
+
+#models: selected using aic for p and q fro script STA_2_models.R with water depth####
+
+TP_outflow_mod <-gls(log(out_tp_c_int)  ~ 
+                       in_tp_c_int+
+                       log1p(tp_rr_int),  
+                     correlation = corARMA(p = 1, q = 0),
+                     data = sta2_int)
+
+
+TP_RR_mod <-gls(log1p(tp_rr_int)  ~ 
+                   in_tp_c_int+
+                   ca_rr_int+
+                   tn_rr_int+ 
+                   in_water_l_int+
+                   por2+
+                   temp_mean_int+
+                   wd_0_mean_int,
+                 correlation = corARMA(p = 0, q = 2),
+                 data = sta2_int, 
+                 method="ML")
+
+TN_RR_mod <-gls(tn_rr_int  ~ 
+                  in_tn_c_int+
+                  ca_rr_int+
+                  in_water_l_int+
+                  por2+
+                  temp_mean_int+
+                  wd_0_mean_int,  
+                correlation = corARMA(p = 1, q = 0),
+                data = sta2_int, 
+                method="ML")
+
+
+
+
+wd_0_mod<-gls(wd_0_mean_int~  
+                por2 + 
+                in_water_l_int+
+                rainfall_mean_int,
+              correlation = corARMA(p = 1, q = 0),
+              data = sta2_int)
+
+
+TCA_RR_mod <-gls(ca_rr_int~  
+                  in_ca_c_int + 
+                  por2 + 
+                  in_water_l_int+
+                  temp_mean_int+
+                  wd_0_mean_int, 
+                data = sta2_int,
+                method="ML")
+
+
+
+
+
+####Path model using psem
+model1<-psem(TN_RR_mod,TCA_RR_mod ,TP_RR_mod,TP_outflow_mod,wd_0_mod)
 summary(model1, .progressBar = F)
 
 #save out coefficents table
@@ -116,87 +175,3 @@ plot(model1, node_attrs = list(
   shape = "rectangle", color = "black",
   fillcolor = "orange", x = 3, y=1:12))
 
-#Direct Effects on microbes
-microbes.table<-mod1_coefs[which(mod1_coefs$Response== "mpd.obs" & mod1_coefs$P.Value <=0.05), ]
-microbes.table
-
-#Direct Effects on midges
-midge.table<-mod1_coefs[which(mod1_coefs$Response== "total_midge" & mod1_coefs$P.Value <=0.05), ]
-midge.table
-
-#Direct Effects on plant morphology
-plant.table<-mod1_coefs[which(mod1_coefs$Response== "plant_PCA" & mod1_coefs$P.Value <=0.05), ]
-plant.table
-
-
-#Direct Effects on plant morphology
-volume.table<-mod1_coefs[which(mod1_coefs$Response== "log1p(volume)" & mod1_coefs$P.Value <=0.05), ]
-volume.table
-
-####Path for MPDz as response#### 
-
-#specify set of models for the path analysis (from best fit models in prelim_glm.R)
-model2<-psem(plant,prey,volume,pH,midge,flies,mosquito,mpdz, total_midge %~~%flesh_flies,
-             total_mosquito%~~%flesh_flies)
-summary(model2, .progressBar = F)
-
-
-
-#save out coefficents table
-mod2_coefs<-coefs(model2)
-write.csv(mod2_coefs, file = "results/mpdz.csv", quote = FALSE, row.names = F)
-
-
-
-#Direct Effects on microbes
-microbes.table<-mod2_coefs[which(mod2_coefs$Response== "mpd.obs.z" & mod2_coefs$P.Value <=0.05), ]
-microbes.table
-
-#Direct Effects on midges
-midge.table<-mod2_coefs[which(mod2_coefs$Response== "total_midge" & mod2_coefs$P.Value <=0.05), ]
-midge.table
-
-#Direct Effects on plant morphology
-plant.table<-mod2_coefs[which(mod2_coefs$Response== "plant_PCA" & mod2_coefs$P.Value <=0.05), ]
-plant.table
-
-
-#Direct Effects on plant morphology
-volume.table<-mod2_coefs[which(mod2_coefs$Response== "log1p(volume)" & mod2_coefs$P.Value <=0.05), ]
-volume.table
-
-
-####Path for chao1 as response#### 
-
-#specify set of models for the path analysis (from best fit models in prelim_glm.R)
-model3<-psem(plant,prey,volume,pH,midge,flies,mosquito,chao,total_midge %~~%flesh_flies,
-             total_mosquito%~~%flesh_flies)
-
-summary(model3, .progressBar = F)
-
-summary(lm(pitch$chao1~pitch$plant_PCA))
-
-plot(lm(log(pitch$Chao_1_closed)~pitch$ph))
-
-#save out coefficents table
-mod3_coefs<-coefs(model3)
-write.csv(mod3_coefs, file = "results/chao_open.csv", quote = FALSE, row.names = F)
-
-
-
-#Direct Effects on microbes
-microbes.table<-mod3_coefs[which(mod3_coefs$Response== "chao1" & mod3_coefs$P.Value <=0.05), ]
-microbes.table
-
-#Direct Effects on midges
-midge.table<-mod3_coefs[which(mod3_coefs$Response== "total_midge" & mod3_coefs$P.Value <=0.05), ]
-midge.table
-
-#Direct Effects on plant morphology
-plant.table<-mod3_coefs[which(mod3_coefs$Response== "plant_PCA" & mod3_coefs$P.Value <=0.05), ]
-plant.table
-
-
-#Direct Effects on plant morphology
-volume.table<-mod3_coefs[which(mod3_coefs$Response== "log1p(volume)" & mod3_coefs$P.Value <=0.05), ]
-volume.table
